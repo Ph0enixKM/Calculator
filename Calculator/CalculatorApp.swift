46c79880d82fd9f493a275721f451963dd13e658
Ph0enixKM <pkaras.it@gmail.com>
2021-07-28 21:02:59 +0200

    Initial Commit

---
+++ Calculator/CalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalculatorApp.swift
+//  Calculator
+//
+//  Created by Paweł Karaś on 28/07/2021.
+//
+
+import SwiftUI
+
+@main
+struct CalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
